2a486f48d5d691f27f459aedef2f74430164622b
KaseyYuen <yuen7347@mydusd.org>
2025-04-14 12:42:58 -0700

    Update ComboMenuv2.py
    not done yet

--- ComboMenuv2.py
+++ ComboMenuv2.py
@@ -8,3 +8,12 @@ Add the following features to your first iteration:
     large $2.25
  Have the program output the user’s beverage size selection, or lack of selection, to verify that the program is working correctly.
  Have the program output the total cost so far.'''
+sSelect = input("chicken $5.25, beef $6.25, tofu $5.75).")
+print(sSelect)
+string1 = "Would you like a beverage?"
+print (string1)
+yesno = input("yes, no).")
+print (yesno)
+if (yesno)input = "no"
+drinksize = input("small $1.00, medium $1.75, large $2.25).")
+print(drinksize)
