6a11df2933cad079e026837801fb6f553cb35824
KaseyYuen <yuen7347@mydusd.org>
2025-04-14 12:18:44 -0700

    Update ComboMenuv1.py
    completed i1

--- ComboMenuv1.py
+++ ComboMenuv1.py
@@ -1,3 +1,5 @@
 '''Iteration 1
  Ask the user for a type of sandwich (chicken $5.25, beef $6.25, tofu $5.75).
  Have the program output the user’s sandwich selection to verify that the program is working correctly.'''
+sSelect = input("chicken $5.25, beef $6.25, tofu $5.75).")
+print(sSelect)
