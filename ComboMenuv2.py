bf80cab37fc66101f557945091d9bf483f2785e1
KaseyYuen <yuen7347@mydusd.org>
2025-04-15 12:42:05 -0700

    Update ComboMenuv2.py
    almost done

--- ComboMenuv2.py
+++ ComboMenuv2.py
@@ -12,8 +12,17 @@ sSelect = input("chicken $5.25, beef $6.25, tofu $5.75).")
 print(sSelect)
 string1 = "Would you like a beverage?"
 print (string1)
-yesno = input("yes, no).")
+(yesno)= input(str("Yes or No"))
 print (yesno)
-if (yesno)input = "no"
-drinksize = input("small $1.00, medium $1.75, large $2.25).")
-print(drinksize)
+if (yesno)==str("Yes" or "yes"):
+  (drinksize) = input("small $1.00, medium $1.75, large $2.25")
+  print(drinksize)
+if (yesno)==str("No" or "no"):
+  string1 = "Would you like fries?"
+  print(string1)
+  (yesno2)= input(str("Yes or No"))
+  print (yesno2)
+  if (yesno2)==str("Yes" or "yes"):
+    (fries) = input("small $1.00, medium $1.50, large $2.00")
+    print(fries)
+  
