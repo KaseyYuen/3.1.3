42ec79335fdd91c4afb9b3f3a6de9cdd2e1e637c
KaseyYuen <yuen7347@mydusd.org>
2025-04-18 12:05:36 -0700

    Update ComboMenuv4.py
    done

--- ComboMenuv4.py
+++ ComboMenuv4.py
@@ -8,7 +8,6 @@ After ordering the sandwich, drink, fries, and ketchup packets:
 order = ["order"]
 numbers = [0]
 total = sum(numbers)
-print(total)
 sSelect = input("chicken $5.25, beef $6.25, tofu $5.75).")
 print(sSelect)
 if (sSelect)==str("chicken" or "Chicken"):
@@ -28,14 +27,14 @@ if (yesno)==str("Yes" or "yes"):
   (drinksize) = input("small $1.00, medium $1.75, large $2.25")
   print(drinksize) 
   if (drinksize)==str("small" or "Small"):
-  total = [total] + [1.00]
-  order = ["order"] + ["small drink"]
+    total = [total] + [1.00]
+    order = ["order"] + ["small drink"]
   if (drinksize)==str("medium" or "Medium"):
-  total = [total] + [1.75]
-  order = ["order"] + ["medium drink"]
+    total = [total] + [1.75]
+    order = ["order"] + ["medium drink"]
   if (drinksize)==str("large" or "Large"):
-  total = [total] + [2.25]
-  order = ["order"] + ["large drink"]
+    total = [total] + [2.25]
+    order = ["order"] + ["large drink"]
   string1 = "Would you like fries?"
   print(string1)
   (yesno2)= input(str("Yes or No"))
@@ -60,10 +59,55 @@ if (yesno)==str("Yes" or "yes"):
       if (yesno)==str("Yes" or "yes"):
         total = [total] + [2.00]
         order = ["order"] + ["large fries"]
+        print(total)
+        print(order)
+        if (sSelect)==str("chicken" or "beef" or "tofu") and (drinksize)==str("small" or "medium" or "large") and (fries)==str("small" or "medium" or "large"):
+          (total) = [total] - [1.00]
+          print(total)
+          print(order)
       if (yesno)==str("No" or "no"):
         total = [total] + [1.00]
         order = ["order"] + ["small fries"]
-        (ketchup) = input(str("How many ketchup packets do you want? $.25 each"))
-        print (ketchup)
         if (sSelect)==str("chicken" or "beef" or "tofu") and (drinksize)==str("small" or "medium" or "large") and (fries)==str("small" or "medium" or "large"):
           (total) = (total) - (1.00)
+if (yesno)==str("No" or "no"):
+  string1 = "Would you like fries?"
+  print(string1)
+  (yesno2)= input(str("Yes or No"))
+  print (yesno2)
+  if (yesno2)==str("Yes" or "yes"):
+    (fries) = input(str("small $1.00, medium $1.50, large $2.00"))
+    print(fries)
+    if (sSelect)==str("small" or "Small"):
+      total = [total] + [1.00]
+      order = ["order"] + ["small fries"]
+    if (sSelect)==str("medium" or "Medium"):
+      total = [total] + [1.50]
+      order = ["order"] + ["medium fries"]
+    if (sSelect)==str("large" or "Large"):
+      total = [total] + [2.00]
+      order = ["order"] + ["large fries"]
+    if (fries)==str("small"):
+      string1 = "Would you like to mega size your fries?"
+      print (string1)
+      (yesno3)= input(str("Yes or No"))
+      print (yesno3)
+      if (yesno)==str("Yes" or "yes"):
+        total = [total] + [2.00]
+        order = ["order"] + ["large fries"]
+        print(total)
+        print(order)
+        if (sSelect)==str("chicken" or "beef" or "tofu") and (drinksize)==str("small" or "medium" or "large") and (fries)==str("small" or "medium" or "large"):
+          (total) = [total] - [1.00]
+          print(total)
+          print(order)
+      if (yesno)==str("No" or "no"):
+        total = [total] + [1.00]
+        order = ["order"] + ["small fries"]
+        if (sSelect)==str("chicken" or "beef" or "tofu") and (drinksize)==str("small" or "medium" or "large") and (fries)==str("small" or "medium" or "large"):
+          (total) = (total) - (1.00)
+  if (yesno2)==str("No" or "no"):
+    print(total)
+    print(order)
+    if (sSelect)==str("chicken" or "beef" or "tofu") and (drinksize)==str("small" or "medium" or "large") and (fries)==str("small" or "medium" or "large"):
+      (total) = [total] - [1.00]
